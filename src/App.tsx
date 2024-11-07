9e9d008a21d5639069687f3b667a18af30f78d2d
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-11-07 11:14:59 -0500

    agregando tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
     return (
         <>
-            <h1>Calculadora de propinas</h1>
+            <h1 className="text-3xl font-bold">Calculadora de propinas</h1>
         </>
     )
 }
